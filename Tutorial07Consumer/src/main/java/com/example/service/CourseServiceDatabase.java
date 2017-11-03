e124080eb672f596bf3eee5cb2ec9269af20a123
riccafitriani <fitrianiricca79@gmail.com>
2017-11-03 18:34:47 +0700

    tutorial07 push latihan 3

---
+++ Tutorial07Consumer/src/main/java/com/example/service/CourseServiceDatabase.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.CourseMapper;
+import com.example.model.CourseModel;
+
+@Service
+public class CourseServiceDatabase implements CourseService
+{
+    @Autowired
+    private CourseMapper courseMapper;
+    
+    @Override
+    public CourseModel selectCourse (String id)
+    {
+        return courseMapper.selectCourse (id);
+    }
+}
